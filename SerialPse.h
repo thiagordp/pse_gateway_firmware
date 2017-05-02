d38f8fe37a10ad05aeefdb081f84d8013a47744a
Thiago Raulino Dal Pont <thiago.dalpont@outlook.com>
2017-05-02 18:49:55 -0300

    implementacao serial arduino

--- SerialPse.h
+++ SerialPse.h
@@ -8,23 +8,31 @@
 #define EPOS    (1)
 #define ARDUINO (0)
 
+#if ARDUINO
+#define PARITY_EVEN     (0b10)
+#define PARITY_ODD      (0b11)
+#define PARITY_NONE     (0b00)
+
+#endif
 
 class SerialPse {
-    
 private:
-  
+
 public:
 
+    /**
+     * 
+     */
     int8_t configure(uint32_t baude_rate, uint32_t data_bits, uint32_t parity, uint32_t stop_bits);
 
-     /**
+    /**
      * Envia o comando especificado de acordo com o padrão RS232 do EPOS
      * @param buf:  Comando enviado
      * @param len:  Comprimento do cmd
      * @return      Resultado da ação
      */
     int8_t send(char *buf, int16_t len);
-    
+
     /**
      * 
      * @param buf
@@ -32,13 +40,13 @@ public:
      * @return 
      */
     int8_t receive(char *buf, int16_t len);
-    
+
     /**
      * 
      * @return 
      */
     int8_t available(void);
-    
+
 };
 
 #endif
